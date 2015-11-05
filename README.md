fbbe0e7ff59f578c812d7a17c867b41a27e2f5ed
Dwayne Thomas <thomas.dwayne@gmail.com>
2015-11-05 10:41:59 -0500

    collision made method of enemy object; readme includes directions on how to get the file

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-frontend-nanodegree-arcade-game
+frontend-nanodegree-arcade-game 11/4/15
 ===============================
+Game was completed to practice javascript **object oriented programming** in the udacity nanodegree front end program.
+
 INSTRUCTIONS:
 
-# To run the game go to http://htmlpreview.github.io/?dwaynethomas/frontend-nanodegree-arcade-game
+* To run the game go to click on the index.html file (dowloads available at the repository: https://github.com/dwaynethomas/frontend-nanodegree-arcade-game)
 
-# Restart the game with a refresh of the browser.
+* Restart the game with a refresh of the browser.
 
-# Player crosses the brick path (with the keyboard's arrow keys)to get the water in order to increase the level of difficulty.
+* Player crosses the brick path (with the keyboard's arrow keys)to get the water in order to increase the level of difficulty.
 
-# Scores and game level are shown in the console log of the game.(command+alt+i keys show the console)
-
+* Scores and game level are shown in the console log of the game.(command+alt+i keys show the console)
 
 ===============================
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
+"use strict";
 
 // TODO: display scores within the browser and not only in the console log
-
 //starting positions for the player, repeated when crosses the screen or
 //collision with enemy
 var START_X_PLAYER = 300;
@@ -14,8 +14,6 @@ var START_Y_PLAYER = 300;
 * @param {image} image of enemy
 *///
 var Enemy = function(x, y, crawlSpeed) {
-    'use strict';
-
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
@@ -27,11 +25,63 @@ var Enemy = function(x, y, crawlSpeed) {
     this.bug = 'images/enemy-bug.png';
 };
 
+
+Enemy.prototype.collision = function (Enemy) {
+      // checks for collision between enemy and player
+      // checks for an overlap of the player and the enemy in any dimension
+      if (
+          player.y + 41 >= this.y &&
+          player.x <= this.x + 63 &&
+          player.y  <= this.y + 62 &&
+          player.x + 65 >= this.x)  {
+          console.log('collided');
+          player.x = START_X_PLAYER;
+          player.y = START_Y_PLAYER;
+      }
+
+      // check for player reaching top of canvas and winning the game
+      // if player wins, add 1 to the level
+      // pass game level as an argument to the increaseDifficulty function
+      if (player.y + 30 <= 0) {
+          player.x = START_X_PLAYER;
+          player.y = START_Y_PLAYER;
+          console.log('wowser, you got across'+ ' you\'re at level ' + gameLevel);
+
+          gameLevel += 1;
+          increaseDifficulty(gameLevel);
+
+      }
+
+      // check if player runs into left, bottom, or right canvas walls
+      // prevent player from moving beyond canvas wall boundaries
+      if (player.y > 383 ) {
+          player.y = 383;
+      }
+      if (player.x > 402.5) {
+          player.x = 402.5;
+      }
+      if (player.x < 0) {
+          player.x = 0;
+      }
+  };
+
+  // Increase number of enemies on screen based on  game Level
+  var increaseDifficulty = function(gameLevel) {
+      // remove all previous enemies on canvas
+      allEnemies.length = 0;
+
+      // load new set of enemies
+      for (var i = 0; i <= (gameLevel-2); i++) {
+          var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+
+          allEnemies.push(enemy);
+      }
+    //statements between lines 108 and 148
+    // Remember to change the anEnemy parameter with the this keyword (e.g. player.y + 41 >= this.y && // Line 114)
+};
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    'use strict';
-
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -43,14 +93,15 @@ Enemy.prototype.update = function(dt) {
     }
 
     // Check for collision with enemies or barrier-walls
-    collision(this);
+    this.collision();
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    'use strict';
     ctx.drawImage(Resources.get(this.bug), this.x, this.y);
 };
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -64,7 +115,6 @@ Enemy.prototype.render = function() {
 **/
 
 var Player = function(x, y, walkSpeed) {
-    'use strict';
     this.x = x;
     this.y = y;
     this.lengthStride = 50;
@@ -75,21 +125,16 @@ var Player = function(x, y, walkSpeed) {
 // change based on time, but input. however it needs to be defined to prevent
 // the game and javascript from breaking.
 Player.prototype.update = function() {
-    'use strict';
 
 };
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
-    'use strict';
-
     ctx.drawImage(Resources.get(this.boy), this.x, this.y);
 };
 
 //Changes the position of the boy on the canvas based on the current location
 Player.prototype.handleInput = function(keyPress) {
-    'use strict';
-
     if (keyPress == 'left') {
         player.x -= player.lengthStride;
     }
@@ -105,61 +150,7 @@ Player.prototype.handleInput = function(keyPress) {
     console.log('keyPress is: ' + keyPress);
 };
 
-var collision = function(anEnemy) {
-    'use strict';
 
-    // checks for collision between enemy and player
-    // checks for an overlap of the player and the enemy in any dimension
-    if (
-        player.y + 41 >= anEnemy.y &&
-        player.x <= anEnemy.x + 63 &&
-        player.y  <= anEnemy.y + 62 &&
-        player.x + 65 >= anEnemy.x)  {
-        console.log('collided');
-        player.x = START_X_PLAYER;
-        player.y = START_Y_PLAYER;
-    }
-
-    // check for player reaching top of canvas and winning the game
-    // if player wins, add 1 to the level
-    // pass game level as an argument to the increaseDifficulty function
-    if (player.y + 30 <= 0) {
-        player.x = START_X_PLAYER;
-        player.y = START_Y_PLAYER;
-        console.log('wowser, you got across'+ ' you\'re at level ' + gameLevel);
-
-        gameLevel += 1;
-        increaseDifficulty(gameLevel);
-
-    }
-
-    // check if player runs into left, bottom, or right canvas walls
-    // prevent player from moving beyond canvas wall boundaries
-    if (player.y > 383 ) {
-        player.y = 383;
-    }
-    if (player.x > 402.5) {
-        player.x = 402.5;
-    }
-    if (player.x < 0) {
-        player.x = 0;
-    }
-};
-
-// Increase number of enemies on screen based on  game Level
-var increaseDifficulty = function(gameLevel) {
-    'use strict';
-
-    // remove all previous enemies on canvas
-    allEnemies.length = 0;
-
-    // load new set of enemies
-    for (var i = 0; i <= (gameLevel-2); i++) {
-        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
-
-        allEnemies.push(enemy);
-    }
-};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -179,8 +170,6 @@ allEnemies.push(enemy);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    'use strict';
-
     var allowedKeys = {
         37: 'left',
         38: 'up',
